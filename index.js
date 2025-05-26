be93058d3599c6d91a53c8d641fc498b3fffa699
Konan Enacle <ratp34687@gmail.com>
2025-05-27 00:12:29 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ]
+});
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    }
+}
+
+client.once('ready', () => {
+    console.log(`${client.user.tag} online.`);
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ 
+            content: 'Une erreur est survenue lors de l\'exécution de la commande', 
+            ephemeral: true 
+        });
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
